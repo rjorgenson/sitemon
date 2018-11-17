d7f79615a4c594507ff64948e407ac03bbdb7bf1
rjorgenson <rjorgenson@gmail.com>
2018-11-17 13:37:01 -0700

    initial commit

---
+++ mon.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var rp = require('request-promise-native');
+
+var monitors = require("./sites.json")
+
+for(var mon in monitors) {
+    rp({ uri: monitors[mon].uri, method: "GET", resolveWithFullResponse: true, "mon": monitors[mon] })
+    .then(function(res) {
+        var regex = new RegExp(res.request.mon.regex)
+        var match = res.body.match(regex)
+        if (match[1] > 0) {
+            console.log(res.request.mon.name+":", match[1])
+        }
+    })
+}
