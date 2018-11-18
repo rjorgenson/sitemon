dc9b8d345375b64093dfb1e68873c662db76c19f
rjorgenson <rjorgenson@gmail.com>
2018-11-17 17:26:00 -0700

    added functionality to track values as they change
    this will writebck the updated value to the config file
    so you will only be alerted once each time the value changes

--- mon.js
+++ mon.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
+
+// load needed modules
 var rp = require('request-promise-native');
+var fs = require('fs');
 
-var monitors = require("./monitors.json")
+// load list of monitors
+var monitors = require("./monitors.json");
 
+// loop through monitors and check their status
 for(var mon in monitors) {
-    rp({ uri: monitors[mon].uri, method: "GET", resolveWithFullResponse: true, "mon": monitors[mon] })
+    rp({ uri: monitors[mon].uri, method: "GET", resolveWithFullResponse: true, "mon": mon, "monData": monitors[mon] })
     .then(function(res) {
-        var regex = new RegExp(res.request.mon.regex)
-        var match = res.body.match(regex)
-        if (match[1] != res.request.mon.curval) {
-            console.log(res.request.mon.name+":", match[1], "-", res.request.mon.uri)
+        var data = res.request.monData
+        var mon = res.request.mon
+        var regex = new RegExp(data.regex);
+        var match = res.body.match(regex);
+        if (match[1] != data.curval) {
+            // output new value with metadata
+            console.log(res.request.monData.name+":", match[1], "-", res.request.monData.uri);
+            // write new value back to monitors.json
+            monitors[mon].curval = match[1];
+            fs.writeFile('./monitors.json', JSON.stringify(monitors, null, 2), function (err) {
+                if (err) return console.log(err)
+            });
         }
-    })
+    });
 }
